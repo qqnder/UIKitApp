b2d9d5ecb5c4583291be6e0e0a8ced64a9df0a7f
qqnder <ruseverus@gmail.com>
2020-10-23 15:46:19 +0300

    1. changing a color of view 2. sliders are working 3. labels are working

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var viewRGB: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabelValue: UILabel!
+    @IBOutlet var greenLabelValue: UILabel!
+    @IBOutlet var blueLabelValue: UILabel!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewRGB.layer.cornerRadius = 10
+        
+        // Setup sliders
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        
+        redSlider.value = 0.76
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        greenSlider.value = 0.26
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        blueSlider.value = 0.49
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        
+        // Setup labels
+        redLabelValue.text = String(format: "%.2f", redSlider.value)
+        greenLabelValue.text = String(format: "%.2f", greenSlider.value)
+        blueLabelValue.text = String(format: "%.2f", blueSlider.value)
+
+        getColorOnTheView()
     }
 
-
+    @IBAction func redSliderAction() {
+        redLabelValue.text = String(format: "%.2f", redSlider.value)
+        getColorOnTheView()
+    }
+    @IBAction func greenSliderAction() {
+        greenLabelValue.text = String(format: "%.2f", greenSlider.value)
+        getColorOnTheView()
+    }
+    @IBAction func blueSliderAction() {
+        blueLabelValue.text = String(format: "%.2f", blueSlider.value)
+        getColorOnTheView()
+    }
+    
+    func getColorOnTheView() {
+        viewRGB.backgroundColor =
+            UIColor(red: CGFloat(redSlider.value),
+                    green: CGFloat(greenSlider.value),
+                    blue: CGFloat(blueSlider.value),
+                    alpha: 1)
+    }
 }
 
